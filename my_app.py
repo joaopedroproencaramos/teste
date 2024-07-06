c1e81a5bc7700b7bfecb5664176811f1f1853e15
RyanSmk <ryansmadeski@gmail.com>
2024-07-06 12:26:40 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,1 +1,45 @@
-# write the code for main app and first screen
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+
+
+from instr import *
+from second_win import *
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    
+    def set_appear(self): 
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+    def initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.hello_text = QLabel( txt_hello )
+        self.instruction = QLabel( txt_instruction )
+        self.button = QPushButton( txt_next )
+        self.layout = QVBoxLayout()
+        self.hello_text.addWidget(self.layout)
+        self.instruction.addWidget(self.layout)
+        self.button.addWidget(self.layout)
+    def connects(self): 
+        self.btn_next.clicked.connect(self.next_click)
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
+
+app = QApplication([])
+mw = MainWin()
+app.exec_()
+
