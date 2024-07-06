46154c50c50f6f35d30b2d34bdbee087a82526f8
TimSilvaSMP <martimbarreirasilva@gmail.com>
2024-07-06 12:32:28 +0100

    Update final_win.py
    banana

--- final_win.py
+++ final_win.py
@@ -1,1 +1,41 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout, QGridLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+      
+from instr import *
+
+
+class FinalWin(QWidget):
+   def __init__(self):
+       ''' the window in which the survey is being conducted '''
+       super().__init__()
+
+
+       # creating and configuring graphic elements:
+      self.initUI()
+
+
+       # sets what the window will look like (label, size, location)
+      self.set_appear()
+      
+       # start:
+      self.show()
+   def set_appear(self)
+      self.Windowtitle(txt_title)
+      self.resize(win_width, win_height)
+      self.move(win_x, win_y)
+
+   def initUI(self):
+       ''' creates graphic elements '''
+      self.workh_text = QLabel(txt_workheart)
+      self.index_text = QLabel(txt_index)
+
+
+      self.layout_line = QVBoxLayout()
+      self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
+      self.layout_line.addWidget(self.workh_text)
+      self.setLayout(self.layout_line)
 # write a code for the third screen of app
